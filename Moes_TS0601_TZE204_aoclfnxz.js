1d8db7585279e9a17fed557e9c410e5a5ea84c18
Alexander <a.burdun@gmail.com>
2024-10-14 02:35:01 +0300

    Add 'program' datapoint

--- Moes_TS0601_TZE204_aoclfnxz.js
+++ Moes_TS0601_TZE204_aoclfnxz.js
@@ -6,6 +6,7 @@ const modernExtend = require('zigbee-herdsman-converters/lib/modernExtend');
 const e = exposes.presets;
 const ea = exposes.access;
 const tuya = require('zigbee-herdsman-converters/lib/tuya');
+//const {logger} = require("../lib/logger");
 
 const exposesLocal = {
     hour: (name) => e.numeric(name, ea.STATE_SET).withUnit('h').withValueMin(0).withValueMax(23),
@@ -62,43 +63,16 @@ const definition = {
         e.temperature_sensor_select(['IN', 'AL', 'OU']),
 
         e.composite('program', 'program', ea.STATE_SET)
-            .withDescription('Time of day and setpoint to use when in program mode')
-            .withFeature(exposesLocal.hour('weekdays_p1_hour'))
-            .withFeature(exposesLocal.minute('weekdays_p1_minute'))
-            .withFeature(exposesLocal.program_temperature('weekdays_p1_temperature'))
-            .withFeature(exposesLocal.hour('weekdays_p2_hour'))
-            .withFeature(exposesLocal.minute('weekdays_p2_minute'))
-            .withFeature(exposesLocal.program_temperature('weekdays_p2_temperature'))
-            .withFeature(exposesLocal.hour('weekdays_p3_hour'))
-            .withFeature(exposesLocal.minute('weekdays_p3_minute'))
-            .withFeature(exposesLocal.program_temperature('weekdays_p3_temperature'))
-            .withFeature(exposesLocal.hour('weekdays_p4_hour'))
-            .withFeature(exposesLocal.minute('weekdays_p4_minute'))
-            .withFeature(exposesLocal.program_temperature('weekdays_p4_temperature'))
-            .withFeature(exposesLocal.hour('saturday_p1_hour'))
-            .withFeature(exposesLocal.minute('saturday_p1_minute'))
-            .withFeature(exposesLocal.program_temperature('saturday_p1_temperature'))
-            .withFeature(exposesLocal.hour('saturday_p2_hour'))
-            .withFeature(exposesLocal.minute('saturday_p2_minute'))
-            .withFeature(exposesLocal.program_temperature('saturday_p2_temperature'))
-            .withFeature(exposesLocal.hour('saturday_p3_hour'))
-            .withFeature(exposesLocal.minute('saturday_p3_minute'))
-            .withFeature(exposesLocal.program_temperature('saturday_p3_temperature'))
-            .withFeature(exposesLocal.hour('saturday_p4_hour'))
-            .withFeature(exposesLocal.minute('saturday_p4_minute'))
-            .withFeature(exposesLocal.program_temperature('saturday_p4_temperature'))
-            .withFeature(exposesLocal.hour('sunday_p1_hour'))
-            .withFeature(exposesLocal.minute('sunday_p1_minute'))
-            .withFeature(exposesLocal.program_temperature('sunday_p1_temperature'))
-            .withFeature(exposesLocal.hour('sunday_p2_hour'))
-            .withFeature(exposesLocal.minute('sunday_p2_minute'))
-            .withFeature(exposesLocal.program_temperature('sunday_p2_temperature'))
-            .withFeature(exposesLocal.hour('sunday_p3_hour'))
-            .withFeature(exposesLocal.minute('sunday_p3_minute'))
-            .withFeature(exposesLocal.program_temperature('sunday_p3_temperature'))
-            .withFeature(exposesLocal.hour('sunday_p4_hour'))
-            .withFeature(exposesLocal.minute('sunday_p4_minute'))
-            .withFeature(exposesLocal.program_temperature('sunday_p4_temperature'))
+            .withDescription(
+                'Schedule will work with "program" preset. In this mode, the device executes ' +
+                'a preset week programming temperature time and temperature. Schedule can contains 12 segments. ' +
+                'All 12 segments should be defined. It should be defined in the following format: "hh:mm/tt". ' +
+                'Segments should be divided by space symbol. ' +
+                'Example: "06:00/20 11:30/21 13:30/22 17:30/23 06:00/24 12:00/23 14:30/22 17:30/21 06:00/19 12:30/20 14:30/21 18:30/20"',
+            )
+            .withFeature(e.text('week_days', ea.STATE_SET).withDescription(''))
+            .withFeature(e.text('saturday', ea.STATE_SET).withDescription(''))
+            .withFeature(e.text('sunday', ea.STATE_SET).withDescription(''))
     ],
     meta: {
         // All datapoints go in here
@@ -118,11 +92,82 @@ const definition = {
             [2, 'running_state', tuya.valueConverterBasic.lookup({idle: tuya.enum(0), heat: tuya.enum(1)})],
             [3, 'preset', tuya.valueConverterBasic.lookup({hold: tuya.enum(0), program: tuya.enum(1)})],
             [43, 'sensor', tuya.valueConverterBasic.lookup({IN: tuya.enum(0), AL: tuya.enum(1), OU: tuya.enum(2)})],
+            [101, 'program', {
+                    to: (v, meta) => {
+                        if (!meta.state.program) {
+                            //logger.warning(`Existing program state not set.`, 'zhc:legacy:tz:moes_bht_002');
+                            return;
+                        }
+
+                        /* Merge modified value into existing state and send all over in one go */
+                        const newProgram = {
+                            // @ts-expect-error ignore
+                            ...meta.state.program,
+                            ...v,
+                        };
+
+                        const regex = /((?<h>[01][0-9]|2[0-3]):(?<m>[0-5][0-9])\/(?<t>[0-3][0-9](\.[0,5]|)))/gm;
+                        let arr;
+                        let matches = [...newProgram.week_days.matchAll(regex)];
+                        if (matches.length === 4) {
+                            arr = matches.reduce((arr, m) => {
+                                arr.push(parseInt(m.groups.h));
+                                arr.push(parseInt(m.groups.m));
+                                arr.push(parseFloat(m.groups.t) * 2);
+                                return arr;
+                            }, []);
+                        }
+
+                        matches = [...newProgram.saturday.matchAll(regex)];
+                        if (matches.length === 4) {
+                            arr = arr.concat(matches.reduce((arr, m) => {
+                                arr.push(parseInt(m.groups.h));
+                                arr.push(parseInt(m.groups.m));
+                                arr.push(parseFloat(m.groups.t) * 2);
+                                return arr;
+                            }, []));
+                        }
+
+                        matches = [...newProgram.sunday.matchAll(regex)];
+                        if (matches.length === 4) {
+                            arr = arr.concat(matches.reduce((arr, m) => {
+                                arr.push(parseInt(m.groups.h));
+                                arr.push(parseInt(m.groups.m));
+                                arr.push(parseFloat(m.groups.t) * 2);
+                                return arr;
+                            }, []));
+                        }
+                        return arr;
+                        //logger.warning('Ignoring invalid or incomplete schedule', NS);
+                    },
+                    from: (v, meta) => {
+                        let r = ['', '', ''];
+
+                        let x = 4;
+                        let y = 0;
+                        for (let i = 0; i < 12; i++) {
+                            r[y] += `${v[i * 3].toString().padStart(2, '0')}:${v[i * 3 + 1].toString().padStart(2, '0')}/${v[i * 3 + 2] / 2}`;
+                            x--;
+
+                            if (x > 0) {
+                                r[y] += ' ';
+                            }
+                            else {
+                                x = 4;
+                                y ++;
+                            }
+                        }
+
+                        return {
+                            week_days:r[0],
+                            saturday:r[1],
+                            sunday:r[2]
+                        };
+                    },
+                },]
         ],
     },
-    extend: [
-
-    ],
+    extend: [],
 };
 
 module.exports = definition;
